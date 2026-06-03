6f8fc289b00d274e64d991d82796d610f691fc9d
MisterKayCodes <kaycris10@gmail.com>
2026-06-04 00:38:43 +0100

    fix: move alert_router to top of registration order

--- app/bot/routers.py
+++ app/bot/routers.py
@@ -3,6 +3,7 @@ BOT: ROUTERS
 Connects all handler modules to the main bot dispatcher.
 """
 from aiogram import Dispatcher, Router
+from app.bot.handlers.alertbot import router as alert_router
 from app.bot.handlers.menu import router as menu_router
 from app.bot.handlers.session import router as session_router
 from app.bot.handlers.pairs import router as pairs_router
@@ -12,10 +13,10 @@ from app.bot.handlers.admin_users import router as admin_users_router
 from app.bot.handlers.admin_settings import router as admin_settings_router
 from app.bot.handlers.logs import router as logs_router
 from app.bot.handlers.stats import router as stats_router
-from app.bot.handlers.alertbot import router as alert_router
 
 
 def register_all_routers(dp: Dispatcher):
+    dp.include_router(alert_router)
     dp.include_router(menu_router)
     dp.include_router(session_router)
     dp.include_router(pairs_router)
@@ -25,4 +26,3 @@ def register_all_routers(dp: Dispatcher):
     dp.include_router(admin_settings_router)
     dp.include_router(logs_router)
     dp.include_router(stats_router)
-    dp.include_router(alert_router)
